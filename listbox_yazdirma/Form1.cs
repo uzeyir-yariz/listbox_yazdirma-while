9663bb80c46f0524cc620c3e4656350cd40b0825
uzeyir-yariz <99393019+uzeyir-yariz@users.noreply.github.com>
2022-02-15 10:54:19 +0200

    Add files via upload

---
+++ listbox_yazdirma/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace listbox_yazdirma
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            listBox1.Items.Clear();
+
+            int i = 0;
+            while(i < Convert.ToInt32(textBox1.Text))
+            {
+                
+                listBox1.Items.Add(textBox2.Text);
+                listBox1.Items.Add(" ");                
+                i++;
+            }
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
